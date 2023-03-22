805494890e60e27d41f2b446a9c420f393a9c368
cth.release <84012697+th-release@users.noreply.github.com>
2023-03-22 14:15:32 +0900

    feat: config

--- backend/src/utils/redis.ts
+++ backend/src/utils/redis.ts
@@ -1,10 +1,14 @@
 import * as redisStore from 'cache-manager-ioredis';
 import { CacheModule } from '@nestjs/common';
-import { REDIS_HOST, REDIS_PORT, REDIS_TTL } from './config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-export const redisModule = CacheModule.register({
-  store: redisStore,
-  host: REDIS_HOST,
-  port: REDIS_PORT,
-  ttl: REDIS_TTL,
+export const redisModule = CacheModule.registerAsync({
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    store: redisStore,
+    host: configService.get<string>('REDIS_HOST'),
+    port: configService.get<number>('REDIS_PORT'),
+    ttl: configService.get<number>('REDIS_TTL')
+  })
 })
